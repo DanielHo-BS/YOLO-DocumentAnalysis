01a6decba91d4747c56bcf4e4752fd467742f58a
DanielHo-BS <danielho1671@gmail.com>
2023-10-02 17:14:36 +0800

    Update detect.bash

--- detect.bash
+++ detect.bash
@@ -1,5 +1,5 @@
 # Detect by yolov7 with new weights
-#python detect.py --weights 0418_FCIoU.pt --save-txt --source ../datasets/old/hand_craft_v10/images/val  --device 0 --project runs/detect/0418 --name old_val
+#python detect.py --weights 0418_FCIoU.pt --save-txt --source ../datasets/old/hand_cra1002_slideft_v10/images/val  --device 0 --project runs/detect/0418 --name old_val
 #python detect.py --weights 0418_FCIoU.pt --save-txt --source ../datasets/old/hand_craft_v10/images/test  --device 0 --project runs/detect/0418 --name old_test
 #python detect.py --weights 0418_FCIoU.pt --save-txt --source ../datasets/dev/jpg  --device 0 --project runs/detect/0418 --name dev_jpg
 #python detect.py --weights 0418_FCIoU.pt --save-txt --source ../datasets/dev/png  --device 0 --project runs/detect/0418 --name dev_png
@@ -10,19 +10,7 @@
 
 
 
-# CIoU
-python detect.py --weights 0223_CIoU.pt --save-txt --source ../datasets/old/hand_craft_v10/images/train  --device 0 --project runs/detect/0223 --name old_train
-python tools/Indicator/indicator_calculator.py --ground-truth-path ../datasets/old/hand_craft_v10/labels/train --prediction-path ./runs/detect/0223/old_train/labels --source ./runs/detect/0223/old_train --save-path ./runs/error/GT_CIOU_train
-python tools/combin_image.py  --path1 ./runs/GT/train --path2 ./runs/error/GT_CIOU_train --save ./runs/out/GT_CIOU_train
-# F-CIoU
-#python detect.py --weights 0329_FCIoU.pt --save-txt --source ../datasets/old/hand_craft_v10/images/train  --device 0 --project runs/detect/0329 --name old_train
-python tools/Indicator/indicator_calculator.py --ground-truth-path ../datasets/old/hand_craft_v10/labels/train --prediction-path ./runs/detect/0329/old_train/labels --source ./runs/detect/0329/old_train --save-path ./runs/error/GT_FCIOU_train
-python tools/combin_image.py  --path1 ./runs/GT/train --path2 ./runs/error/GT_FCIOU_train --save ./runs/out/GT_FCIOU_train
-# EIoU
-python detect.py --weights 0404_EIoU.pt --save-txt --source ../datasets/old/hand_craft_v10/images/train  --device 0 --project runs/detect/0404 --name old_train
-python tools/Indicator/indicator_calculator.py --ground-truth-path ../datasets/old/hand_craft_v10/labels/train --prediction-path ./runs/detect/0404/old_train/labels --source ./runs/detect/0404/old_train --save-path ./runs/error/GT_EIOU_train
-python tools/combin_image.py  --path1 ./runs/GT/train --path2 ./runs/error/GT_EIOU_train --save ./runs/out/GT_EIOU_train
-# F-EIoU
-python detect.py --weights 0307_FEIoU.pt --save-txt --source ../datasets/old/hand_craft_v10/images/train  --device 0 --project runs/detect/0307 --name old_train
-python tools/Indicator/indicator_calculator.py --ground-truth-path ../datasets/old/hand_craft_v10/labels/train --prediction-path ./runs/detect/0307/old_train/labels --source ./runs/detect/0307/old_train --save-path ./runs/error/GT_FEIOU_train
-python tools/combin_image.py  --path1 ./runs/GT/train --path2 ./runs/error/GT_FEIOU_train --save ./runs/out/GT_FEIOU_train
+# CIoU with ca
+python detect.py --weights 1002_CA.pt --save-txt --source ../datasets/old/hand_craft_v10/images/val  --device 0 --project runs/detect/1002 --name ca
+python tools/Indicator/indicator_calculator.py --ground-truth-path ../datasets/old/hand_craft_v10/labels/val --prediction-path ./runs/detect/1002/ca/labels --source ./runs/detect/1002/ca --save-path ./runs/error/1002_ca
+python tools/combin_image.py  --path1 ./runs/GT/val --path2 ./runs/error/1002_ca --save ./runs/out/1002_slide
