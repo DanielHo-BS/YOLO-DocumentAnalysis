8f4e254fa95e52a9efe765c62c28844e946e06af
Pika <danielho1671@gmail.com>
2023-11-24 15:39:39 +0800

    Init yolov8

--- augraphy/__init__.py
+++
@@ -1,6 +0,0 @@
-from augraphy.augmentations import *
-from augraphy.base import *
-from augraphy.default import *
-from augraphy.utilities import *
-
-__version__ = "8.2.4"
--- augraphy/default/__init__.py
+++
@@ -1,8 +0,0 @@
-from augraphy.default.pipeline import default_augment
-from augraphy.default.pipeline import default_augraphy_pipeline
-
-
-__all__ = [
-    "default_augraphy_pipeline",
-    "default_augment",
-]
